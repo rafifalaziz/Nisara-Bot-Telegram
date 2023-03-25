ee1cfea9935ef845aa400a5e0f03721b601936a7
rafifalaziz <mrafifalaziz26@gmail.com>
2023-03-25 11:43:49 +0700

    add inlinekeyboard button

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const {Telegraf} = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM);
 
 bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'hello! Welcome to Nisara, ' + ctx.from.first_name, {
-    })
+    ctx.reply('hello! Welcome to Nisara, '
+        + ctx.from.first_name
+        + ' \nWhat can i help you?',
+        Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Hotline Kekerasan Rumah Tangga', 'rumahtangga')],[
+                Markup.button.callback('Hotline Kekerasan Seksual', 'seksual')],[
+                Markup.button.callback('Konsultasi Dokter', 'dokter')],[
+                Markup.button.callback('Konsultasi Peer Counselor', 'counselor')],[
+                Markup.button.callback('Tentang Nisara', 'aboutus')]
+            ]
+        )
+    )
 })
+
+bot.action('rumahtangga', (ctx) => {
+    ctx.reply('Hotline kekerasan rumah tangga');
+});
+
+bot.action('seksual', (ctx) => {
+    ctx.reply('Hotline kekerasan seksual');
+});
+
+bot.action('dokter', (ctx) => {
+    ctx.reply('Konsultasi dokter');
+});
+
+bot.action('counselor', (ctx) => {
+    ctx.reply('Konsultasi peer counselor');
+});
+
+bot.action('aboutus', (ctx) => {
+    ctx.reply('Tentang Nisara');
+});
 
 bot.launch();
